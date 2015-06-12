f3526b16d77ae1a5eb034ec913df3cf8fb6d7863
Satia Herfert <satiaherfert@gmx.de>
2015-06-12 21:41:36 +0200

    Added data classes

--- src/main/java/data/EqType.java
+++ src/main/java/data/EqType.java
@@ -1,5 +1,56 @@
 package data;
 
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class EqType {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Exercise> availableExercises;
+	
+	@Basic
+	private String name;
+	
+	public EqType() {}
+	
+	public EqType(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public List<Exercise> getAvailableExercises() {
+		return availableExercises;
+	}
+
+	public void setAvailableExercises(List<Exercise> availableExercises) {
+		this.availableExercises = availableExercises;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
--- src/main/java/data/Session.java
+++ src/main/java/data/Session.java
@@ -1,5 +1,56 @@
 package data;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Session {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST, optional = false)
+	private Exercise exercise;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST, optional = false)
+	private Equipment equipment;
+	
+	public Session() {}
+	
+	public Session(Exercise exercise, Equipment equipment) {
+		this.exercise = exercise;
+		this.equipment = equipment;
+	}
+
+
+
+	public Exercise getExercise() {
+		return exercise;
+	}
+
+	public void setExercise(Exercise exercise) {
+		this.exercise = exercise;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Equipment getEquipment() {
+		return equipment;
+	}
+
+	public void setEquipment(Equipment equipment) {
+		this.equipment = equipment;
+	}
 
 }
