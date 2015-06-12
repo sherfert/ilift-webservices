b482bd46510cd34223d47c4d11df959d24cde3bf
Satia Herfert <satiaherfert@gmx.de>
2015-06-12 20:42:46 +0200

    Dummy DataManager

---
+++ src/main/java/persistence/DataManager.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import java.util.List;
+
+import data.Equipment;
+import data.Session;
+import data.User;
+
+public class DataManager {
+	
+	public static Equipment getEquipmentById(long id) {
+		return null;
+	}
+	
+	public static void insertSession(/*TODO*/) {
+		
+	}
+	
+	public static User getUserbyId(long id) {
+		return null;
+	}
+	
+	public static List<Session> getSessionOfUserById(long id) {
+		return null;
+	}
+
+}
